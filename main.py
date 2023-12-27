518644f37abaa78261c810f90195f94344703459
Kells_P <kellsping@gmail.com>
2023-12-27 15:15:08 +0800

    Update function.
    Assign the language and region for search options.

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import requests
 API_KEY = open('./API_KEY').read()
 SEARCH_ENGINE_ID = open('./SEARCH_ENGINE_ID').read()
 
-search_query = 'fruit apple'
+search_query = '蘋果'
 
 url = 'https://www.googleapis.com/customsearch/v1'
 params = {
     'q':search_query,
     'key':API_KEY,
     'cx':SEARCH_ENGINE_ID,
-    'search_type':'image'
+    'search_type':'image',
+    'lr':'lang_en',
+    'gl':'US'
 }
 
 response = requests.get(url, params=params)
